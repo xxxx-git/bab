3b8ead13877e28d81ba9a10d2cc3eeb015065b90
Unknown <unknown@example.com>
2019-07-03 17:07:02 +0300

    feat: set config
    - change structure of config for full control by 
      configuration about how jwt generate
    - security - now our service do not know anything about how user look like

--- service/Shared/Config/ITokenSettings.cs
+++ service/Shared/Config/ITokenSettings.cs
@@ -1,5 +1,26 @@
 namespace Shared.Config {
     public interface ITokenSettings {
         string Secret { get; set; }
+        IHeaderSettings Headers { get; set; }
+        IClaimSettings Claims { get; set;}
+    }
+
+    public interface IHeaderSettings {
+        string Type {get; set;}
+        string Algorithm {get; set;}
+    }
+
+    public interface IClaimSettings {
+        string Issuer { get; set; }
+        string Audience { get; set; }
+        string Subject { get; set; }
+        IExpiresClaimSettings Expires { get; set; }
+        string User { get; set; }
+    }
+
+    public interface IExpiresClaimSettings {
+        int Months {get; set;}
+        int Days {get; set;}
+        int Hours {get; set;}
     }
 }
