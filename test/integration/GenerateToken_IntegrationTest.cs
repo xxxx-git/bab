ac6c9289f3dd2af0d4cd4c62342196bb1026fc79
Unknown <unknown@example.com>
2019-07-29 15:10:31 +0300

    wip

---
+++ test/integration/GenerateToken_IntegrationTest.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace test.integration
+{
+    public class GenerateToken_IntegrationTest
+    {
+        [Theory]
+        [MemberData()]
+        public void GenerateToken_ShouldGenerateToken()
+        {
+            
+        }
+    }
+}
